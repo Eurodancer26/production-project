22c04cab810bb4c7fa9284745b9a50bc74238dca
Eurodancer26 <murushkin.timur@gmail.com>
2023-12-25 14:57:48 +0300

    feature-sliced.design

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import { FC, useMemo, useState } from "react";
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "../lib/ThemeContext";
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
@@ -7,9 +7,7 @@ const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || T
 const ThemeProvider: FC = ({children}) => {
     const [theme, setTheme] = useState<Theme>(defaultTheme);
 
-    // const toggleTheme = () => {
-    //     setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
-    // }
+
 
     const defaultProps = useMemo(() => ({
         theme: theme,
